fe9daade36f9eb620ca8f19a51b8dc1eba194fca
SujithChristopher <chrisbon95@gmail.com>
2023-12-08 16:46:23 +0530

    added new changes

--- analysis/support.py
+++ analysis/support.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.signal import interp1d
+from scipy.interpolate import interp1d
+import polars as pl
 
 def slice_data(df):
     
@@ -19,41 +20,44 @@ def slice_data(df):
     _temp_df = _temp_df[_start_inx:_end_inx]   
     return _temp_df
 
-def interpolate(df, ref_df):
+def interpolate(ref_df, target_df):
     # df is the df to be interpolated
     # ref_df is the reference df
     # df and ref_df should have the same columns
-    
-    columns = df.columns[1:-1]
-    
-    interp_func = []
+
+    _reference_time = ref_df['timestamp']
+
+    columns = target_df.columns[1:-1]
+    interpolated_cols = {}
     for _col in columns:
-        interp_func.append(interp1d(ref_df['time'], ref_df[columns], kind='linear', axis=0, fill_value='extrapolate'))
-    
-    for i in range(len(df['time'])):
-        for j in range(len(columns)):
-            df[columns[j]][i] = interp_func[j](df['time'][i])
-    
+        interp_func = interp1d(target_df['timestamp'], target_df[_col], kind='linear', axis=0, fill_value='extrapolate')
+        interpolated_cols[_col] = interp_func(_reference_time)
+        
+    df = pl.from_dict(interpolated_cols).insert_column(0, _reference_time)
     return df
 
 def interpolats_dfs(dfs, ref_no = 0):
     # generally first df is the reference df
+    _keys = list(dfs.keys())
+    _reference_df = dfs[_keys[ref_no]]
+    _temp_dfs = dfs.copy()
+    _temp_dfs.pop(_keys[ref_no])
+    _selected_keys = _temp_dfs.keys()    
+    _new_dfs = {}
+    for _key in _selected_keys:
+        _new_dfs[_key] = interpolate(_reference_df, dfs[_key]) 
+        
+    _new_dfs[_keys[ref_no]] = _reference_df
     
-    _reference_df = dfs[ref_no]
-    columns = _reference_df.columns[1:-1]
-    
-    interp_func = []
-    for _col in columns:
-        interp_func.append(interp1d(_reference_df['time'], _reference_df[columns], kind='linear', axis=0, fill_value='extrapolate'))
-    
-    
-    return dfs
-    
-    
-    
+    return _new_dfs
 
 def preprocess(dfs):
     for _name in dfs.keys():
         dfs[_name] = slice_data(dfs[_name])
         
+    dfs = interpolats_dfs(dfs)
+        
+    return dfs
+
+def apply_median_filter(dfs):
     return dfs
